e248328a9bddac45cc28baa0135491cae401d6d0
gc-mcinnes <rory.mcinnes@nrcan-rncan.gc.ca>
2025-09-24 14:00:47 -0300

    Updated jurisdiction functions and fixed BC prep. Updated the outputs

--- caribouLocPrep.R
+++ caribouLocPrep.R
@@ -16,7 +16,7 @@ defineModule(sim, list(
                   'reproducible', 'move','dplyr', 'terra'),
   parameters = bindrows(
     defineParameter("urlToBCDataFolder", "character",
-                    "https://drive.google.com/file/d/1isb5qtXhHEt728F_isunXVnefOIiIgny/view?usp=drive_link", NA, NA,
+                    "https://drive.google.com/drive/u/0/folders/1spylo1XDqYO1SmvRXfzhIo3v6rsSryZs", NA, NA,
                     desc = "The URL to a folder where BC data is kept"),
     defineParameter("urlToSKDataFolder", "character",
                     "https://drive.google.com/drive/folders/18jdIk_62S73PRe1ZtX0JXxp1pxxbFPaX", NA, NA,
@@ -24,6 +24,9 @@ defineModule(sim, list(
     defineParameter("urlToMBDataFolder", "character",
                     "https://drive.google.com/drive/folders/1hdTIcpiMueewC1DZsJE1gW8R8mdG7CkF", NA, NA,
                     desc = "The URL to a folder where MB data is kept"),
+    defineParameter("urlToONDataFolder", "character",
+                    "https://drive.google.com/drive/u/0/folders/1pivxW9sNOvoVNecsDH-rTuqP6WyI1XTF", NA, NA,
+                    desc = "The URL to a folder where ON data is kept"),
     defineParameter("NWTMovebankID", "character",
                     "", NA, NA,
                     desc = "The MoveBank study ID where NTW is kept"),
@@ -36,7 +39,7 @@ defineModule(sim, list(
     defineParameter("MoveBankPass", "character",
                     "", NA, NA,
                     desc = "The MoveBank user password to access studies stored on MoveBank"),
-    defineParameter("jurisdiction", "character",c("BC","SK","MB","YT"),
+    defineParameter("jurisdiction", "character",c("BC","SK","MB", "ON", "NT", "YT"),
                     desc = "A list of jurisdictions to run"),
     defineParameter(".plots", "character", "screen", NA, NA,
                     "Used by Plots function, which can be optionally used here"),
@@ -86,6 +89,7 @@ doEvent.caribouLocPrep = function(sim, eventTime, eventType) {
 
 Init <- function(sim) {
   #harmonize the jurisdictions provided and remove erroneous points
+  browser()
   dat.all <- list()
   colnames <- c(BC = "Population_Unit", SK = "SK1", MB = "Range", YT = "Yukon")
   subpops <- list(BC = NA, SK = NA, MB = NA, NT = "habitat", YT = NA)
@@ -103,20 +107,19 @@ Init <- function(sim) {
   })
   dat.bind <- do.call("rbind",dat.all)
   ### remove crazy points in Russia (??) ----
-  dat.clean <- dat.bind[complete.cases(x,y, datetime) & between(x, -1665110, 0) &between(y, -98940, 2626920)]
-  
+  dat.clean <- dat.bind[complete.cases(x,y, datetime)& between(x, -3894480, 4432820) &between(y, -1229436, 4329766)]
   # Save clean data ----
   sim$booALL <- dat.clean
   # create study area and buffered study area ----
-  coords<- dat.clean%>%st_as_sf(coords = c('x','y'))%>%
+  sfcoords<- dat.clean%>%st_as_sf(coords = c('x','y'))%>%
     st_set_crs(st_crs(3978))
   
   ## 100 km buffer around points to get an idea of extent of study area 
   ## A convex hull of all the polygons creates a single polygon for the simulation
-  ## This requires a lot of computing power, run the script on a HPC
-  studyArea.buff <- st_buffer(coords, dist = 100000)
+  studyArea.buff <- st_buffer(sfcoords, dist = 100000)
   studyArea.sf <- st_as_sf(studyArea.buff)
-  studyArea.ch <- st_convex_hull(studyArea.sf)
+  studyArea.union <- st_union(studyArea.sf)
+  studyArea.ch <- sf::st_convex_hull(studyArea.union)
   
   sim$studyareaFullextent <- studyArea.ch
   ## save buffered study area ----
@@ -134,16 +137,16 @@ Init <- function(sim) {
   if (!suppliedElsewhere("boo", sim = sim)) {
     sim$boo <- list()
     if ("BC" %in% Par$jurisdiction == TRUE) {
-      #download just the .gdb files from google drive
-      layers <- c("KMB_Local_Telemetry_20211026", "Regional_Telemetry_20211026")
+      #download just the .gdb file from google drive
+      layers <- list("Regional_Telemetry_20211026", "KMB_Local_Telemetry_20211026")
       telemDataZipFile <- "telem_data_request_20211026.gdb.zip"
       bc_layers <- list()
       bc_layers <- Map(layer = layers, function(layer) {
         prepInputs(url = Par$urlToBCDataFolder,
-                   targetFile = telemDataZipFile,
+                   targetFile = telemDataZipFile, useCache = FALSE, 
+                   layer = layer,
                    destinationPath = dPath, fun = terra::vect(x = targetFile, layer = layer))
       })
-      
       sim$boo[["BC"]] <- prepInputs(destinationPath = dPath,
                                     fun = dataPrep_BC(dPath=dPath, bc_layers, layers))
     }
@@ -154,11 +157,20 @@ Init <- function(sim) {
                                     fun = dataPrep_SK(dPath = dPath))
     }
     if ("MB" %in% Par$jurisdiction == TRUE){
-      #download the .gdb files (there are many so this prepInputs takes more time)
+      #download the .gdb files (there are many, this prepInputs takes time)
       sim$boo[["MB"]] <- prepInputs(url = Par$urlToMBDataFolder,
                                     destinationPath = dPath,
-                                    archive = NA,
+                                    archive = NA, 
                                     fun = dataPrep_MB(dPath = dPath))
+    }
+    if ("ON" %in% Par$jurisdiction == TRUE){
+      #download the ontario data
+      ontario_zip <- "BorealCaribou_NHICMedSensitive2025.gdb.zip"
+      sim$boo[["ON"]] <- prepInputs(url = Par$urlToONDataFolder,
+                                    targetFile = ontario_zip,
+                                    destinationPath = dPath,
+                                    archive = NA,
+                                    fun = dataPrep_ON(dPath = dPath))
     }
     if ("YT" %in% Par$jurisdiction == TRUE){
       #use movebank to access the data
@@ -168,7 +180,7 @@ Init <- function(sim) {
     if ("NT" %in% Par$jurisdiction == TRUE){
       #use movebank to access the data
       print("Access to NT data requires a Move Bank account, ensure you have collaboration rights to the study")
-      sim$boo[["NWT"]] <- prepInputs(fun = dataPrep_NT(loginStored))
+      sim$boo[["NT"]] <- prepInputs(fun = dataPrep_NT(loginStored))
     }
   }
   return(invisible(sim))
@@ -230,8 +242,8 @@ dataPrep_SK <- function(dPath =dpath) {
 
 dataPrep_BC <- function(dPath=dPath, bc_layers, layers) {
   ### Input data ----
-  kmb <- bc_layers[[layers[1]]]
-  regional <- bc_layers[[layers[2]]]
+  kmb <- bc_layers[[2]]
+  regional <- bc_layers[[1]]
   
   ### Prep data ----
   # checking for right formats and grabbing what need
@@ -274,37 +286,33 @@ dataPrep_BC <- function(dPath=dPath, bc_layers, layers) {
   
 }
 
-#no movebank data here
 dataPrep_NT <- function(loginStored) {
   ### Input data ----
-  browser()
   # NWT dataset names to fill in loop
   dsNames <-c('Dehcho Boreal Woodland Caribou', 'North Slave Boreal Caribou', 
               'Sahtu Boreal Woodland Caribou', 'Sahtu Boreal Woodland Caribou (2020)',
               'South Slave Boreal Woodland Caribou') 
   # make a list of all data from Movebank
-  # TODO: update this with prepInputs()
   nt.move <- list()
   for (ds in 1:length(dsNames)) {
     #hh = 1
     nt.move[[ds]]<-getMovebankLocationData(study =paste0( 'GNWT ', dsNames[[ds]]),
                                            login = loginStored, removeDuplicatedTimestamps=TRUE)
   }
-  #saveRDS(nwt.move, paste0(raw.nwt, 'NWTmoveDat.RDS'))
   
   # pull just the data
-  hab <-c('dehcho', 'north.slave', 'sahtu', 'south.slave') 
+  hab <-c('dehcho', 'north.slave', 'sahtu', 'sahtu2020', 'south.slave') 
   
-  
+  nt <- data.table()
   nt <- rbindlist(lapply(1:length(hab), function(hh){
-    nt[,.(area=hab[[hh]], dat=list(setDT(nt.move[[hh]]@data)))]
+    nt[,.(area=hab[[hh]], dat=list(setDT(nt.move[[hh]])))]
   })
   )
   
   # gathering just the columns needed
   nt.long <- rbindlist(lapply(1:length(hab), function(hh){
-    nt$dat[[hh]][,.(area = hab[[hh]], habitat, id=tag_id, 
-                    location_long, location_lat, datetime = timestamp)]
+    nt$dat[[hh]][,.(area = hab[[hh]], habitat, id=tag.id, 
+                    location.long, location.lat, datetime = timestamp)]
     
   }))
   
@@ -312,10 +320,10 @@ dataPrep_NT <- function(loginStored) {
   crs <- CRS(st_crs(4326)$wkt)
   outcrs <- st_crs(3978)
   
-  sfboo <- st_as_sf(nt.long, coords = c('location_long', 'location_lat'),
+  sfboo <- st_as_sf(nt.long, coords = c('location.long', 'location.lat'),
                     crs = crs)
   outboo <- st_transform(sfboo, outcrs)
-  sim$booNT <- setDT(sfheaders::sf_to_df(outboo, fill = T))
+  booNT <- setDT(sfheaders::sf_to_df(outboo, fill = T))
   return(booNT)
 }
 
@@ -386,6 +394,8 @@ dataPrep_MB <- function(dPath = dPath) {
   wabowden2 <- st_read(file.path(raw.mb, 'CaribouGPSdata_Wabowden.gdb.zip'), "Wabowden_UHF_relocations_2009to2012_NAD83")
   wabowden3 <- st_read(file.path(raw.mb, 'CaribouGPSdata_Wabowden.gdb.zip'), "Wabowden_GPS_MBHYDRO_Q4_2010_022021")
   
+  kamuchawie <- st_read(file.path(raw.mb, 'CaribouGPSData_Kamuchawie.gdb.zip'), "Positions2025Jun12_Kamuchawie")
+  
   ### Prep data ----
   ### convert to NAD83/Canada Atlas Lambert (need units to be m and consistent across)
   outcrs <- st_crs(3978)
@@ -425,6 +435,8 @@ dataPrep_MB <- function(dPath = dPath) {
   out_wabowden1 <- st_transform(wabowden1, outcrs)
   out_wabowden2 <- st_transform(wabowden2, outcrs)
   out_wabowden3 <- st_transform(wabowden3, outcrs)
+  
+  out_kamuchawie <- st_transform(kamuchawie, outcrs)
   
   
   # checking for right formats and grabbing what need
@@ -603,17 +615,43 @@ dataPrep_MB <- function(dPath = dPath) {
                                                         format = '%Y-%m-%d %H:%M:%OS'),
                                   x, y)]
   
+  DT_kamuchawie <- setDT(sfheaders::sf_to_df(out_kamuchawie, fill = T))
+  DT_kamuchawie <- DT_kamuchawie[,.(id = Collar_ID, Fix_Status = Fix_Type, Range = 'Kamuchawie', 
+                                    datetime = as.POSIXct(Acq__Time__UTC_, format = '%Y-%m-%d %H:%M:%OS'),
+                                    x, y)]
+  
   mb.dt <- rbind(DT_ATIKO, DT_BERENS_RND, DT_BLDVN, DT_bog1, DT_bog2, DT_charron1, DT_charron2,
                  DT_flintstone, DT_harding1, DT_harding2, DT_interlake, DT_kississing, DT_MBhydro,
                  DT_MBhydro, DT_MI_Berens, DT_MI_NWH, DT_naosap1, DT_naosap2, DT_naosap3,
                  DT_wabowden1, DT_wabowden2, DT_wabowden3, DT_wheadon1, DT_wheadon2, DT_wheadon3,
-                 DT_william, DT_wimwap1, DT_wimwap2)
+                 DT_william, DT_wimwap1, DT_wimwap2, DT_kamuchawie)
   
   # right now quick and dirty way to deal with outliers
-  dat_cleaner <- mb.dt[complete.cases(x,y, datetime) & x < 0]
+  dat_cleaner <- mb.dt[complete.cases(x,y, datetime)]
   booMB <- dat_cleaner
   
   return(booMB)
 }
 
-#add ON
+dataPrep_ON <- function(dPath = dPath) {
+  raw.on <- dPath
+  
+  on.pts <- st_read(file.path(raw.on, 'BorealCaribou_NHICMedSensitive2025.gdb.zip'), "Species_TRK_Pts")
+  
+  outcrs <- st_crs(3978)
+  
+  out_on.pts <- st_transform(on.pts, outcrs)
+  
+  on.dt <- setDT(sfheaders::sf_to_df(out_on.pts, fill = T))
+  
+  #this needs to be ont custom
+  on.dt <- on.dt[,.(id = IDENT, LOCATION_FIX_TYPE, Range = "Ontario", datetime = as.POSIXct(MONITOR_DATETIME,
+                                                                                            tz = 'gmt', format = '%Y-%m-%d %H:%M'),
+                    x, y)]
+  
+  dat_cleaner <- on.dt[complete.cases(x,y, datetime)]
+  booON <- dat_cleaner
+  
+  return(booON)
+  
+}
